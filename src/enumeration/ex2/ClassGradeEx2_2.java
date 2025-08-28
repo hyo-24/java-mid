4e002e6e26916b0aa77d9b8bbf1a9f0cf76b9f9a
Elena Lee <tbfjs3976@icloud.com>
2025-08-28 15:24:11 +0900

    8/27

---
+++ src/enumeration/ex2/ClassGradeEx2_2.java
@@ -0,0 +1,14 @@
+package enumeration.ex2;
+
+public class ClassGradeEx2_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+
+        ClassGrade newClassGrade = new ClassGrade(); // 상수를 가진 클래스의 객체
+        int result = discountService.discount(newClassGrade,price);
+        System.out.println("newClassGrade 등급의 할인 금액: "+result);
+    }
+}
