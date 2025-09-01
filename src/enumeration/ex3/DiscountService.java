dfcb034daa2d8d5f95eef28a1bbb254cf0be624e
Elena Lee <tbfjs3976@icloud.com>
2025-09-01 12:26:02 +0900

    8/30

--- src/enumeration/ex3/DiscountService.java
+++ src/enumeration/ex3/DiscountService.java
@@ -6,7 +6,7 @@ public class DiscountService {
     public int discount(Grade grade, int price) { // 메서드
         int discountPercent = 0;
 
-        if (grade == Grade.BASIC) {
+        if (grade == Grade.BASIC) { // 등급으로 할인율을 정한다.
             discountPercent = 10;
         } else if (grade == Grade.GOLD) {
             discountPercent = 20;
