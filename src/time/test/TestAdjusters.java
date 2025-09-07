6883b795436814c1900e370c48effd68aae86bf1
Elena Lee <tbfjs3976@icloud.com>
2025-09-07 21:07:58 +0900

    9/7

--- src/time/test/TestAdjusters.java
+++ src/time/test/TestAdjusters.java
@@ -12,9 +12,11 @@ public class TestAdjusters {
         int month = 1;
 
         LocalDate date = LocalDate.of(year, month, 1);
-        DayOfWeek firstDayOfWeek = date.getDayOfWeek(); // DayOfWeek 은 요일을 나타내는 열거형이다.
+        DayOfWeek firstDayOfWeek = date.getDayOfWeek();
         DayOfWeek lastDayOfWeek = date.with(TemporalAdjusters.lastDayOfMonth()).getDayOfWeek();
-        // TemporalAdjusters 는 날짜와 시간을 조작할 수 있는 클래스다. with와 같이 쓴다.
+        // TemporalAdjusters 는 날짜와 시간을 읽을 수 있는 클래스다.
+        // with() 메서드는 날짜와 시간의 특정 필드를 변경할 수 있는 메서드다.
+        // date 의 날짜를 읽어서 그 달의 마지막날을 반환하고, 요일을 확인한다.
 
         System.out.println("firstDayOfWeek = "+firstDayOfWeek);
         System.out.println("lastDayOfWeek = "+lastDayOfWeek);
